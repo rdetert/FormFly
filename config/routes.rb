3163a8b3332728d7c0f8bb37a8f6e6a48278ddff
rdetert <ryan.detert@gmail.com>
2011-01-06 04:02:26 -0800

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Formfly::Application.routes.draw do
   
-  match '/media(/:dragonfly)', :to => Dragonfly[:images]
+  # match '/media(/:dragonfly)', :to => Dragonfly[:images]
   
   post '/posts/ajax_photo_destroy/(:slot)' => "posts#ajax_photo_destroy", :as => :destroy_photo_posts
   resources   :posts do
