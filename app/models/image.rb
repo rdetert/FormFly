36b6042a0a51dcd6405340c6dce0e71e2ed958cc
rdetert <ryan.detert@gmail.com>
2011-01-06 21:07:27 -0800

    checks for mime types now

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,5 @@ class Image < Asset
   set_table_name "images"
   image_accessor :data
   validates_size_of :data, :maximum =>  500.kilobytes
+  validates_property :mime_type, :of => :data, :in => %w(image/jpeg image/jpg image/png image/gif)
 end
